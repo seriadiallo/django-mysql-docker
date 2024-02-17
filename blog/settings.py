76a4fafd21e9782054e67135e956b6e94c0f3647
Sadialiou Diallo <seriadiallo1@gmail.com>
2024-02-17 12:57:44 +0000

    ajout de la configuration pour depoiement avec nginx

--- blog/settings.py
+++ blog/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+"""
+ les images de  profile
+"""
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 # Default primary key field type
